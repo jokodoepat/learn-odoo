e5a9c66201312e52a13e64836dfc0e5c14745703
jokodoepat <jkuncoro28@gmail.com>
2025-02-24 11:51:50 +0700

    contact approval

---
+++ contact_approval/contact_approval/models/contact_approval.py
@@ -0,0 +1,37 @@
+from odoo import models, fields, api, _
+from datetime import date
+from odoo.exceptions import UserError, ValidationError
+
+class ContactApproval(models.Model):
+    _inherit = 'res.partner'
+
+    # ================ Non Relational Data ================
+
+    state = fields.Selection(
+        [
+            ("draft","Draft"),
+            ("approved","Approved"),
+            ("canceled","Canceled")
+        ],
+        string="State",
+        default="draft"
+    )
+
+    # ================ Relational Data ================
+
+    approver_id = fields.Many2one('res.users', string='Approved By')
+
+    # ================ Function ================
+
+    def action_approve(self):
+        for rec in self:
+            rec.approver_id = self.env.user
+            rec.state = 'approved'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'canceled'
+
+    def action_reset(self):
+        for rec in self:
+            rec.state = 'draft'
